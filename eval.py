aa5c54f7ace9c7a8ad2b66e2c0a12aebe5d203fb
Titus von Köller <titus.von.koeller@container-solutions.com>
2018-05-17 14:51:30 +0200

    implemented obj-oriented version and variable binding

--- eval.py
+++ eval.py
@@ -6,26 +6,50 @@ from collections import Callable
 
 class Node:
     def __init__(self, value, *children):
-        self.value = value
+        self.value    = value
         self.children = children
     def __repr__(self):
         return f'Node({self.value}, *{self.children})'
+    def __call__(self, env=None):
+        return self.value(*[child(env=env) for child in self.children])
 
-printr = lambda *a, **kw: print(*a, **kw) # does not evaluate
-non_recursing = [printr]
+class Atom(Node):
+    def __init__(self, value):
+        super().__init__(value)
+    def __call__(self, env=None):
+        return self.value
+
+class Printr(Node):
+    def __init__(self, *children):
+        super().__init__(print, *children)
+    def __call__(self, env=None):
+        return self.value(*self.children)
+
+class Suite(Node):
+    def __call__(self, env=None):
+        for child in self.children:
+            child(env=env)
+
+class Unbound:
+    def __init__(self, *children):
+        self.children = children
+    def __call__(self, env=None):
+        if env is None:
+            env = {**DEFAULT_ENV}
+        value, *children = self.children
+        if children:
+            node = env[value](*children)
+        else:
+            node = env[value]
+        return node(env=env)
+
+DEFAULT_ENV = {'printr' : Printr, }
 
 # (print (+ 1 1) (+ 2 2))
-x = Node(add, Node(1), Node(1))
-y = Node(add, Node(2), Node(2))
+x = Node(add, Unbound('x'), Atom(1))
+y = Node(add, Atom(2), Atom(2))
 n = Node(print, x, y)
-p = Node(print, Node(printr, n), n)
-
-def evaluate(AST):
-    if isinstance(AST.value, Callable):
-        if AST.value not in non_recursing:
-            return AST.value(*[evaluate(child) for child in AST.children])
-        return AST.value(*AST.children)
-    return AST.value
+p = Suite(None, Unbound('printr', n), n)
     
-evaluate(p)
+p(env={**DEFAULT_ENV, 'x':Atom(101)})
 
