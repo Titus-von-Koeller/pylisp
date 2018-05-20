a002229fe913979544203e35c4e18d767a7ed0f0
Titus von Köller <titus.von.koeller@container-solutions.com>
2018-05-20 07:02:43 +0200

    began user function

--- nodes.py
+++ nodes.py
@@ -63,3 +63,144 @@ class Node:
     def __call__(self, env):
         raise NotImplementedError()
 
+class Comment(Node):
+    @debug
+    def __call__(self, env):
+        return Nil
+
+class Suite(Node):
+    @debug
+    def __call__(self, env):
+        rv = Nil
+        for child in self.children:
+            rv = child(env)
+        return rv
+
+class Setq(Node):
+    @debug
+    def __call__(self, env):
+        name, value = self.name(env), self.value(env)
+        env[name.value] = value
+        return value
+    def __init__(self, name, value):
+        super().__init__(name, value)
+    name  = property(lambda self: self.children[0])
+    value = property(lambda self: self.children[1])
+
+class List(Node):
+    @debug
+    def __call__(self, env):
+        values = [val(env) for val in self.values]
+        values.reverse()
+        rv = Cell(values[0], Nil)
+        for v in values[1:]:
+            rv = Cell(v, rv)
+        return rv
+    def __init__(self, car, *cdr):
+        super().__init__(car, *cdr)
+    values = property(lambda self: self.children)
+
+class Cons(Node):
+    @debug
+    def __call__(self, env):
+        car, cdr = self.car(env), self.cdr(env)
+        return Cell(car, cdr)
+    def __init__(self, car, cdr):
+        super().__init__(car, cdr)
+    car = property(lambda self: self.children[0])
+    cdr = property(lambda self: self.children[1])
+
+class Car(Node):
+    @debug
+    def __call__(self, env):
+        return self.cons(env).car
+    def __init__(self, cons):
+        super().__init__(cons)
+    cons = property(lambda self: self.children[0])
+
+class Cdr(Node):
+    @debug
+    def __call__(self, env):
+        return self.cons(env).cdr
+    def __init__(self, cons):
+        super().__init__(cons)
+    cons = property(lambda self: self.children[0])
+
+class Cell(Node):
+    @debug
+    def __call__(self, env):
+        return self
+    def __init__(self, car, cdr):
+        super().__init__(car, cdr)
+    car   = property(lambda self: self.children[0])
+    cdr   = property(lambda self: self.children[1])
+    value = property(lambda self: (self.car.value, self.cdr.value))
+
+class ProgramError(Exception):
+    pass
+
+class Assert(Node):
+    @debug
+    def __call__(self, env):
+        if not self.cond(env).value:
+            raise ProgramError(self.msg(env).value)
+        return Nil
+    def __init__(self, cond, msg):
+        super().__init__(cond, msg)
+    cond = property(lambda self: self.children[0])
+    msg  = property(lambda self: self.children[1])
+
+class Eq(Node):
+    @debug
+    def __call__(self, env):
+        left, right = self.left(env), self.right(env)
+        left, right = left.value, right.value # unwrap
+        return Atom(left == right) # wrap
+    def __init__(self, left, right):
+        super().__init__(left, right)
+    left  = property(lambda self: self.children[0])
+    right = property(lambda self: self.children[1])
+
+class Print(Node):
+    @debug
+    def __call__(self, env):
+        args = [arg(env) for arg in self.args]
+        args = [arg.value for arg in args] # unwrap
+        return Atom(print(*args)) # wrap
+    def __init__(self, *args):
+        super().__init__(*args)
+    args = property(lambda self: self.children)
+
+class Name(Node):
+    @debug
+    def __call__(self, env):
+        return self
+    def __init__(self, name):
+        super().__init__(name)
+    name  = property(lambda self: self.children[0])
+    value = name
+
+class Var(Node):
+    @debug
+    def __call__(self, env):
+        return env[self.name]
+    def __init__(self, name):
+        super().__init__(name)
+    name = property(lambda self: self.children[0])
+
+class Atom(Node):
+    @debug
+    def __call__(self, env):
+        return self
+    def __init__(self, value):
+        super().__init__(value)
+    value = property(lambda self: self.children[0])
+
+class Nil(Node):
+    def __call__(self, env):
+        return self
+    value = property(lambda self: None)
+Nil = Nil()
+
+class UfuncBase(Node):
+    pass
